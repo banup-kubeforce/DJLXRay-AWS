23784c18a7d78db9416c2c51b14c3adf221c19cd
banup-kubeforce <102495939+banup-kubeforce@users.noreply.github.com>
2022-09-22 09:07:51 -0500

    Initial commit

---
+++ src/main/java/com/kubeforce/djlxrayaws/DjlxRayAwsApplication.java
@@ -0,0 +1,13 @@
+package com.kubeforce.djlxrayaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DjlxRayAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DjlxRayAwsApplication.class, args);
+	}
+
+}
